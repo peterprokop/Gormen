9c2de7caff0c3c04142ffffbb36d860de90701d3
Peter Prokop <prokop.petr@gmail.com>
2016-02-21 11:36:54 +0100

    Add problems to 2.3

--- 2.3-merge-sort.go
+++ 2.3-merge-sort.go
@@ -78,6 +78,9 @@ func mergeSortInternal(A []int, p int, r int) []int {
 	return A
 }
 
+// Problems
+
+// 2-2
 func bubbleSort(A []int) []int {
 	for i := 0; i < len(A)-1; i++ {
 		for j := len(A) - 1; j >= i+1; j-- {
@@ -91,3 +94,22 @@ func bubbleSort(A []int) []int {
 
 	return A
 }
+
+// 2-4
+
+// a. List the fivei nversions of the array <2;3;8;6;1>
+// Answer:
+// (1,5) (2,5) (3,5) (4,5) (3,4)
+
+// b. What array with elements from the set {1; 2; ... ; n}
+// has the most inversions? How many does it have?
+// Answer:
+// Array sorted in reverse order. {n; ...; 2; 1}
+// It's easy to show that it has (n-1) + (n-2) + ... + 1 =
+// = n*(n-1)/2 inversions
+
+// c. What is the relationship between the running time of
+// insertion sort and the number of inversions in the input array? Justify your answer.
+// Answer:
+// Let `I` be the number of inversions and `n` number of elements in the array.
+// It can be shown that running time of insertion sort is `Theta(I + n)`
