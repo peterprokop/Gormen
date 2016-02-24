66a1cfaaa193cd9fa2a5174e1c85594ee98624a5
Peter Prokop <pitrowech@gmail.com>
2016-02-24 18:31:46 +0100

    Add 5.1-randomized-algs

--- 5.1-randomized-algs.go
+++ 5.1-randomized-algs.go
@@ -41,3 +41,10 @@ func random(a, b int) int {
 		}
 	}
 }
+
+// TODO: 5.1-3
+// Suppose that you want to output 0 with probability 1=2 and 1 with probability 1=2.
+// At your disposal is a procedure BIASED-RANDOM, that outputs either 0 or 1.
+// It outputs 1 with some probability p and 0 with probability 1-p, where 0 < p < 1, but you do not know what p is.
+// Give an algorithm that uses BIASED-RANDOM as a subroutine, and returns an unbiased answer, returning 0 with probability 1/2
+// and 1 with probability 1/2. What is the expected running time of your algorithm as a function of p?
